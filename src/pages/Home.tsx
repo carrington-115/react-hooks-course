4de14d6452bc14c6b0e23192828c24aeb4f39811
carrington-115 <frumarkcarrington@gmail.com>
2025-01-14 18:56:12 +0530

    built a counter application that functionos by using the useState variable to change the value of a counter of an app using a button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 export default function componentName() {
+  const [value, setValue] = useState<number>(0);
+
+  const handleAddValue = () => {
+    setValue((prev) => {
+      return (prev += 1);
+    });
+  };
+
+  const handleReduceValue = () => {
+    setValue((preve) => {
+      return (preve -= 1);
+    });
+  };
   return (
     <>
       <div>
-        <h2>Hello World React hooks course</h2>
+        <h2>counter Application</h2>
+        <button onClick={handleAddValue}>Add</button>
+        <span>{value}</span>
+        <button onClick={handleReduceValue}>Minus</button>
       </div>
     </>
   );
