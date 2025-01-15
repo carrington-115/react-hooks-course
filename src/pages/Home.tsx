7e819782dca8aeaf73dae48ab4ff66e01705b2c8
carrington-115 <frumarkcarrington@gmail.com>
2025-01-15 08:10:39 +0530

    built the counter application to work for the both static and dynamic incrementing and decrementing of the counter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export default function componentName() {
   const [value, setValue] = useState<number>(0);
+  const [incrementValue, setIncrementValue] = useState<number>(1);
 
   const handleAddValue = () => {
     setValue((prev) => {
-      return (prev += 1);
+      return incrementValue === 0 ? (prev += 1) : (prev += incrementValue);
     });
   };
 
   const handleReduceValue = () => {
-    setValue((preve) => {
-      return (preve -= 1);
+    setValue((prev) => {
+      return incrementValue === 0 ? (prev -= 1) : (prev -= incrementValue);
     });
   };
+
+  useEffect(() => {}, []);
   return (
     <>
-      <div>
+      <CounterContainer>
         <h2>counter Application</h2>
-        <button onClick={handleAddValue}>Add</button>
-        <span>{value}</span>
-        <button onClick={handleReduceValue}>Minus</button>
-      </div>
+        <section>
+          <button onClick={handleAddValue}>Add</button>
+          <span>{value}</span>
+          <button onClick={handleReduceValue}>Minus</button>
+        </section>
+        <form action="">
+          <fieldset>
+            <label htmlFor="by-value">Increacse by value:</label>
+            <input
+              type="number"
+              value={incrementValue}
+              onChange={(e) => setIncrementValue(Number(e.target.value))}
+            />
+          </fieldset>
+        </form>
+      </CounterContainer>
     </>
   );
 }
+
+const CounterContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  section {
+    display: flex;
+    gap: 10px;
+  }
+`;
