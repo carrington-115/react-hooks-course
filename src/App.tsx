12f3443403e948b1fe062d7b7a06dc67da3b6996
carrington-115 <frumarkcarrington@gmail.com>
2025-01-15 18:56:15 +0530

    started the react context

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,29 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Posts from "./pages/posts";
 import styled from "styled-components";
+import React, { useState } from "react";
+
+export const ThemeContext = React.createContext(false);
+
 function App() {
+  const [dark, setDark] = useState(false);
+
+  const toggleTheme = () => setDark((prevTheme) => !prevTheme);
   return (
     <>
-      <Router>
-        <HeaderContainer>
-          <Link to="/">Home</Link>
-          <Link to="/posts">dynamic</Link>
-        </HeaderContainer>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-        </Routes>
-      </Router>
+      <ThemeContext.Provider value={dark}>
+        <Router>
+          <HeaderContainer>
+            <Link to="/">Home</Link>
+            <Link to="/posts">dynamic</Link>
+            <button onClick={toggleTheme}>Toggle Theme</button>
+          </HeaderContainer>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/posts" element={<Posts />} />
+          </Routes>
+        </Router>
+      </ThemeContext.Provider>
     </>
   );
 }
